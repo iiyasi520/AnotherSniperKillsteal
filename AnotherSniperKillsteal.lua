080c81bdee9f40994decaf31dca7fa1319800789
spiregor <spiregor25201@gmail.com>
2017-05-01 12:53:17 +0300

    Rework Killsteal

--- AnotherSniperKillsteal.lua
+++ AnotherSniperKillsteal.lua
@@ -2,6 +2,7 @@
 local AnotherAutoAssassinate = {}
 
 AnotherAutoAssassinate.IsToggled = Menu.AddOption( {"Hero Specific","Sniper"}, "Another Auto Assassinate", "Auto use ultimate if target killable")
+AnotherAutoAssassinate.optionKey = Menu.AddKeyOption({"Hero Specific","Sniper"},"Another Auto Assassinate Key",Enum.ButtonCode.KEY_D)
 AnotherAutoAssassinate.sleepers = {}
 AnotherAutoAssassinate.Modifiers = { [0] = "modifier_medusa_stone_gaze_stone", [1] = "modifier_winter_wyvern_winters_curse", [2] = "modifier_item_lotus_orb_active", [3] = "modifier_templar_assassin_refraction_absorb", [4] = "modifier_item_blade_mail_reflect", [5] = "modifier_nyx_assassin_spiked_carapace" }
 
@@ -13,8 +14,10 @@ function AnotherAutoAssassinate.OnUpdate()
 	if not Ultimate or not Ability.IsReady(Ultimate) or not Ability.IsCastable(Ultimate, Ability.GetManaCost(Ultimate)) then return end
 	local target = AnotherAutoAssassinate.FindTarget(hero, Ultimate)
 	if not target then return end
+	if Menu.IsKeyDown(AnotherAutoAssassinate.optionKey) then
 	Ability.CastTarget(Ultimate, target)	
 	AnotherAutoAssassinate.Sleep(0.1, "updaterate");
+	end
 end
 
 function AnotherAutoAssassinate.FindTarget(me, ability)
@@ -26,12 +29,13 @@ function AnotherAutoAssassinate.FindTarget(me, ability)
 	if NPC.GetItem(me, "item_dragon_lance", true) or NPC.GetItem(me, "item_hurricane_pike", true) then
 	DragonLance = 140
 	end
-	local dagondmg = UltimateDamage + UltimateDamage * (Hero.GetIntellectTotal(me) / 16 / 100)
+	--Log.Write(NPC.GetAttackRange(me))
+	local AbsDMG = UltimateDamage + UltimateDamage * (Hero.GetIntellectTotal(me) / 16 / 100)
 	local entities = Heroes.GetAll()
 	for index, ent in pairs(entities) do
 		local enemyhero = Heroes.Get(index)
-		if not Entity.IsSameTeam(me, enemyhero) and not NPC.IsLinkensProtected(enemyhero) and not NPC.IsIllusion(enemyhero) and NPC.IsEntityInRange(me, enemyhero, Ability.GetCastRange(ability) + NPC.GetCastRangeBonus(me)) and not NPC.IsEntityInRange(me, enemyhero, NPC.GetAttackRange(me) + Ability.GetLevelSpecialValueFor(NPC.GetAbilityByIndex(me, 2), "bonus_attack_range")+DragonLance) then
-			local totaldmg = (1 - NPC.GetMagicalArmorValue(enemyhero)) * dagondmg
+		if not Entity.IsSameTeam(me, enemyhero) and not NPC.IsLinkensProtected(enemyhero) and not NPC.IsIllusion(enemyhero) and NPC.IsEntityInRange(me, enemyhero, Ability.GetCastRange(ability) + NPC.GetCastRangeBonus(me)) and not NPC.IsEntityInRange(me, enemyhero, NPC.GetAttackRange(me) + DragonLance) then
+			local totaldmg = (1 - NPC.GetMagicalArmorValue(enemyhero)) * AbsDMG
 			local isValid = AnotherAutoAssassinate.CheckForModifiers(enemyhero)
 			if Entity.GetHealth(enemyhero) < totaldmg and isValid then return enemyhero end
 		end
